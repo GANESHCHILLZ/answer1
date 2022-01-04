82b0b873fb58a6595ed1e26dfc07c2852d3edbce
GANESHCHILLZ <95964859+GANESHCHILLZ@users.noreply.github.com>
2022-01-04 17:18:51 +0530

    Add files via upload

---
+++ answers.js
@@ -0,0 +1,9 @@
+
+/*1) completed 
+
+2) HTTP2 is much faster and more reliable than HTTP1.
+   HTTP1 loads single request for every TCP connection .
+
+ 3) Objects is JS is most important data type and forms the buildig blocks for modern js.  
+
+ */
